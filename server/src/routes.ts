879d51d611677d6b46c6c7988f54c7c614395f92
Fabricyo Barreto <bass.barreto@gmail.com>
2020-08-12 14:55:55 -0300

    The route that lists a specific collection point was made

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,59 +1,16 @@
 import express from 'express';
-import db from './database/connection';
+
+import ItemsController from './controllers/ItemsController';
+import PointsController from './controllers/PointsController';
 
 const routes = express.Router();
 
-routes.get('/items', async (request, response) => {
-  const items = await db('items').select('*');
+const itemsController = new ItemsController();
+const pointsController = new PointsController();
 
-  const serializedItems = items.map((item) => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`
-    };
-  });
+routes.get('/items', itemsController.index);
 
-  return response.json(serializedItems);
-});
-
-routes.post('/points', async (request, response) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items
-  } = request.body;
-
-  const trx = await db.transaction();
-
-  const insertedIds = await trx('points').insert({
-    image: 'image-fake',
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf
-  });
-
-  const point_id = insertedIds[0];
-
-  const pointItems = items.map((item_id: number) => {
-    return {
-      item_id,
-      point_id
-    };
-  });
-
-  await trx('point_items').insert(pointItems);
-
-  return response.json({ success: true });
-});
+routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
