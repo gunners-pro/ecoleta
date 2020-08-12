97ea5fe8ae398c5308ff6331fbf926998ff5f375
Fabricyo Barreto <bass.barreto@gmail.com>
2020-08-12 18:53:29 -0300

    The application's route file was configured and the route from the initial page to the collection point registration page was made

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import './styles';
+
+const CreatePoint: React.FC = () => {
+  return (
+    <>
+      <h1>CreatePoint</h1>
+    </>
+  );
+};
+
+export default CreatePoint;
