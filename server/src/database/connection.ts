072fbc061b698877e968b0655ae209ca427d8a39
Fabricyo Barreto <bass.barreto@gmail.com>
2020-08-11 23:22:25 -0300

    A route has been configured that returns a list with the title and image_url of the static images

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,10 +1,12 @@
 import knex from 'knex';
 import path from 'path';
 
-const connection = knex({
+const db = knex({
   client: 'sqlite3',
   connection: {
     filename: path.resolve(__dirname, 'database.sqlite')
   },
   useNullAsDefault: true
 });
+
+export default db;
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import db from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Olá Mundo' });
+routes.get('/items', async (request, response) => {
+  const items = await db('items').select('*');
+
+  const serializedItems = items.map((item) => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    };
+  });
+
+  return response.json(serializedItems);
 });
 
 export default routes;
