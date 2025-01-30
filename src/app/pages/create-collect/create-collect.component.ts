1f45797db7679c8d209cb55fe93d28d943497124
Fabricyo Barreto <bass.barreto@gmail.com>
2025-01-30 17:41:07 -0300

    style: add header to create-collect component

---
+++ src/app/pages/create-collect/create-collect.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-create-collect',
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './create-collect.component.html',
+  styleUrl: './create-collect.component.css'
+})
+export class CreateCollectComponent {
+
+}
