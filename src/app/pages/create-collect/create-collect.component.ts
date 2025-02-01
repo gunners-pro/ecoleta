74a64568f105849ffe25504e06425ffb9bd97056
Fabricyo Barreto <bass.barreto@gmail.com>
2025-02-01 16:53:23 -0300

    feat: add pick image function

--- src/app/pages/create-collect/create-collect.component.ts
+++ src/app/pages/create-collect/create-collect.component.ts
@@ -8,5 +8,22 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './create-collect.component.css'
 })
 export class CreateCollectComponent {
+  imageUrl: string | ArrayBuffer | null | undefined = null
+
+  onChangeFile(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files) {
+      const file: File = input.files[0];
+
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          this.imageUrl = e.target?.result;
+        }
+
+        reader.readAsDataURL(file);
+      }
+    }
+  }
 
 }
